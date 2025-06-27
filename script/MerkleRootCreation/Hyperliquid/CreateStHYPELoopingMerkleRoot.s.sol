d30814b1801ac5b36e4b00bce7b8d1bac557f871
Rhein0712 <165314392+Rhein0712@users.noreply.github.com>
2025-06-26 20:09:31 -0400

    merkle root creation first version

---
+++ script/MerkleRootCreation/Hyperliquid/CreateStHYPELoopingMerkleRoot.s.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {FixedPointMathLib} from "@solmate/utils/FixedPointMathLib.sol";
+import {ERC20} from "@solmate/tokens/ERC20.sol";
+import {Strings} from "lib/openzeppelin-contracts/contracts/utils/Strings.sol";
+import {MerkleTreeHelper} from "test/resources/MerkleTreeHelper/MerkleTreeHelper.sol";
+import "forge-std/Script.sol";
+
+contract CreateStHYPELoopingMerkleRoot is Script, MerkleTreeHelper {
+    using FixedPointMathLib for uint256;
+
+    address public boringVault = 0xYourVault;
+    address public managerAddress = 0x5FB7587be4c51E56163e4A2ee1E9393DC2d1a361;
+    address public accountantAddress = 0x567fca0423b8fb84E151Cd2fA954555D2323622e;
+    address public decoder = 0x831D9337Eb3926A3C1869145C967E3B9Ec4d24A0;
+
+    function run() external {
+        generateStHYPELoopMerkleRoot();
+    }
+
+    function generateStHYPELoopMerkleRoot() public {
+        setSourceChainName(hyperliquid); // or arbitrum, etc.
+        setAddress(false, hyperliquid, "boringVault", boringVault);
+        setAddress(false, hyperliquid, "managerAddress", managerAddress);
+        setAddress(false, hyperliquid, "accountantAddress", accountantAddress);
+        setAddress(false, hyperliquid, "decoder", decoder);
+
+        ManageLeaf ;
+        uint256 i;
+
+        // 1. Approve wHYPE -> stHYPE staking
+        leafs[i] = ManageLeaf(
+            getAddress(hyperliquid, "wHYPE"),
+            false,
+            "approve(address,uint256)",
+            new address ,
+            "Approve stHYPE staking contract to spend wHYPE",
+            decoder
+        );
+        leafs[i++].argumentAddresses[0] = getAddress(hyperliquid, "stHYPEStaking");
+
+        // 2. Stake wHYPE into stHYPE
+        leafs[i++] = ManageLeaf(
+            getAddress(hyperliquid, "stHYPEStaking"),
+            false,
+            "stake(uint256)",
+            new address ,
+            "Stake wHYPE into stHYPE",
+            decoder
+        );
+
+        // 3. Approve stHYPE to Felix
+        leafs[i] = ManageLeaf(
+            getAddress(hyperliquid, "stHYPE"),
+            false,
+            "approve(address,uint256)",
+            new address ,
+            "Approve Felix market to spend stHYPE",
+            decoder
+        );
+        leafs[i++].argumentAddresses[0] = getAddress(hyperliquid, "FelixVanillaMarket");
+
+        // 4. Supply stHYPE as collateral
+        leafs[i++] = ManageLeaf(
+            getAddress(hyperliquid, "FelixVanillaMarket"),
+            false,
+            "supplyCollateral(uint256)",
+            new address ,
+            "Supply stHYPE as collateral",
+            decoder
+        );
+
+        // 5. Borrow HYPE from Felix
+        leafs[i++] = ManageLeaf(
+            getAddress(hyperliquid, "FelixVanillaMarket"),
+            false,
+            "borrow(uint256)",
+            new address ,
+            "Borrow HYPE",
+            decoder
+        );
+
+        // 6. Approve HYPE to wHYPE wrapper
+        leafs[i] = ManageLeaf(
+            getAddress(hyperliquid, "HYPE"),
+            false,
+            "approve(address,uint256)",
+            new address ,
+            "Approve wrapper to wrap HYPE",
+            decoder
+        );
+        leafs[i++].argumentAddresses[0] = getAddress(hyperliquid, "wHYPEWrapper");
+
+        // 7. Wrap HYPE to wHYPE
+        leafs[i++] = ManageLeaf(
+            getAddress(hyperliquid, "wHYPEWrapper"),
+            false,
+            "wrap(uint256)",
+            new address ,
+            "Wrap HYPE to wHYPE",
+            decoder
+        );
+
+        _verifyDecoderImplementsLeafsFunctionSelectors(leafs);
+
+        bytes32[][] memory tree = _generateMerkleTree(leafs);
+        _generateLeafs("./leafs/Hyperliquid/stHYPELoopingMerkleTree.json", leafs, tree[tree.length - 1][0], tree);
+    }
+}
